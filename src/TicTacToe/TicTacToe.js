7254db3413f42678eec0c2b06c7c87a66a9e40a0
tomthomas1 <tomthomas8688@gmail.com>
2022-05-23 09:28:39 +0530

    Implemented the Tic-Tac-Toe game with React

---
+++ src/TicTacToe/TicTacToe.js
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+import "./TicTacToe.css";
+
+const TicTacToe = () => {
+  const [turn, setTurn] = useState("X");
+  const [cells, setCells] = useState(Array(9).fill(""));
+  const [winner, setWinner] = useState();
+
+  const checkForWinner = (squares) => {
+    let combos = {
+      across: [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+      ],
+      down: [
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+      ],
+      diagonal: [
+        [0, 4, 8],
+        [2, 4, 6],
+      ],
+    };
+
+    for (let combo in combos) {
+      combos[combo].forEach((pattern) => {
+        if (
+          squares[pattern[0]] === "" ||
+          squares[pattern[1]] === "" ||
+          squares[pattern[2]] === ""
+        ) {
+          //do nothing
+        } else if (
+          squares[pattern[0]] === squares[pattern[1]] &&
+          squares[pattern[1]] === squares[pattern[2]]
+        ) {
+          setWinner(squares[pattern[0]]);
+        }
+      });
+    }
+  };
+
+  const Cell = ({ num }) => {
+    return <td onClick={() => handleClick(num)}>{cells[num]}</td>;
+  };
+
+  const handleClick = (num) => {
+    if (cells[num] !== "") {
+      alert("already clicked");
+      return;
+    }
+    let squares = [...cells];
+
+    if (turn === "X") {
+      squares[num] = "X";
+      setTurn("O");
+    } else {
+      squares[num] = "0";
+      setTurn("X");
+    }
+    checkForWinner(squares);
+    setCells(squares);
+    console.log(squares);
+  };
+
+  const handleRestart = () => {
+    setWinner(null);
+    setCells(Array(9).fill(""));
+  };
+
+  return (
+    <div className="container">
+      <table>
+        <h2> Turn: {turn} </h2>
+        <tbody>
+          <tr>
+            <Cell num={0} />
+            <Cell num={1} />
+            <Cell num={2} />
+          </tr>
+          <tr>
+            <Cell num={3} />
+            <Cell num={4} />
+            <Cell num={5} />
+          </tr>
+          <tr>
+            <Cell num={6} />
+            <Cell num={7} />
+            <Cell num={8} />
+          </tr>
+        </tbody>
+      </table>
+      {winner && (
+        <>
+          <h3>{winner} is the winner!</h3>
+          <button onClick={() => handleRestart()}>Play Again</button>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default TicTacToe;
